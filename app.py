fcfd15bfe91d5060e57f103391f285d6b9c94c8e
mann <mannbhatnagar256@gmail.com>
2026-06-29 06:52:12 +0530

    Switch to PostgreSQL

--- app.py
+++ app.py
@@ -47,8 +47,24 @@ def inject_user():
 with app.app_context():
     db.create_all()
     with app.app_context():
-        print("Connected successfully!")
-        print(User.query.count())
+
+        db.create_all()
+
+        if User.query.count() == 0:
+
+            admin = User(
+                cpf="12345",
+                name="Administrator",
+                password="12345",
+                role="ADMIN"
+            )
+
+            db.session.add(admin)
+            db.session.commit()
+
+            print("Admin user created.")
+
+        print("Users:", User.query.count())
     print("Tables created")
 
 
